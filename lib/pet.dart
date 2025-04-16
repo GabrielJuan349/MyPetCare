070a95596d76f58763af49252a8c32d94632eabd
DaBM17 <93148240+DaBM17@users.noreply.github.com>
2025-04-16 19:21:07 +0200

    Create pet.dart

---
+++ lib/pet.dart
@@ -0,0 +1,5 @@
+class Pet {
+  late String name, gender, age, breed, image;
+  late int playful, friendly;
+  Pet(this.name, this.gender, this.age, this.breed, this.image, this.playful, this.friendly);
+}
