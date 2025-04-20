09cc833c767610f2bb02cd0362416c5b838cc5ac
Eric Sánchez Ibáñez de Zuazo <ericsiz2003@gmail.com>
2025-04-20 19:28:08 +0200

    Autentificacion

---
+++ MyPetCare-api/interfaces/user.interface.ts
@@ -0,0 +1,11 @@
+export interface User {
+    email: string;
+    password: string;
+    userId: string;
+    accountType: string;
+    firstName: string;
+    lastName: string;
+    phone: string;
+    clinicInfo: string;
+  }
+  
--- MyPetCare-api/main.ts
+++ MyPetCare-api/main.ts
@@ -1,6 +1,9 @@
 import { Application, Router } from 'oak';
 import {oakCors} from 'cors';
 import { load } from 'dotenv';
+import { authenticate } from "./api-functions/authenticate.ts";
+import { validateToken } from './api-functions/validateToken.ts';
+import { registerUser } from './api-functions/registerUser.ts';
 
 
 await load({ export: true });
@@ -17,13 +20,16 @@ app.use(oakCors({
 }));
 
 router
-  .post('/api/endpoint', (ctx) => {
-    console.log(ctx.request.body({ type: 'json' }));
-  })
+  .post("/api/authenticate", authenticate)
+
+  // Ruta para validar el token
+  .post("/api/validateToken", validateToken)
+
+  .post("/api/registerUser", registerUser); 
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-const port = Number(Deno.env.get("API_PORT"));
+const port = Number(Deno.env.get("API_PORT")) || 8000; 
 app.listen({ port });
 console.log(`Server running at http://localhost:${port}`);
