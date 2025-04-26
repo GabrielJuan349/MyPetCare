6457deff74352135f6fc9a2da0c03eed3606fbd1
Xinyu Yu <solyu041127@gmail.com>
2025-04-26 16:43:20 +0200

    Update updateUser + deleteUser y endpoints para mantener mismo formato

--- MyPetCare-api/main.ts
+++ MyPetCare-api/main.ts
@@ -1,4 +1,4 @@
-import { Application, Router} from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import { load } from 'dotenv';
@@ -6,7 +6,7 @@ import { authenticate } from "./api-functions/authenticate.ts";
 import { validateToken } from './api-functions/validateToken.ts';
 import { registerUser } from './api-functions/registerUser.ts';
 
-import {updateUser, deleteUser} from "./api-functions/gestion.usuarios.ts"
+import { updateUser, deleteUser } from "./api-functions/gestion.usuarios.ts"
 
 await load({ export: true });
 
@@ -28,27 +28,17 @@ router
   .post("/api/validateToken", validateToken)
 
   .post("/api/registerUser", registerUser)
-//UPDATE user info
-  .put("/user/:user_id", async (ctx)=>{
-  const userId = ctx.params.user_id; //Get id from the endpoint
-  const fields = await ctx.request.body({type: "json"}).value;
-  const res = await updateUser(userId, fields);
-  ctx.response.status = res.status;
-  ctx.response.body = res.message;
-})
+  
+  //UPDATE user info
+  .put("/user/:user_id", updateUser)
 
-//DELETE user by id
-  .delete("/user/:user_id", async (ctx)=>{
-  const userId = ctx.params.user_id;
-  const res = await deleteUser(userId);
-  ctx.response.status = res.status;
-  ctx.response.body = res.message;
-})
+  //DELETE user by id
+  .delete("/user/:user_id", deleteUser)
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-const port = Number(Deno.env.get("API_PORT")) || 8000; 
+const port = Number(Deno.env.get("API_PORT")) || 8000;
 app.listen({ port });
 console.log('PORT:', port);
 console.log(`Server running at http://localhost:${port}`);
