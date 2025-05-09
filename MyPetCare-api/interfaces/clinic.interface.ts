6c07ca47275e8c619490d1ae3b80c9afd4882123
Eric Sánchez Ibáñez de Zuazo <ericsiz2003@gmail.com>
2025-05-09 16:59:45 +0200

    Interface clinic

---
+++ MyPetCare-api/interfaces/clinic.interface.ts
@@ -0,0 +1,11 @@
+export interface ClinicInfo {
+  address: string;
+  categories: string[];
+  city: string;
+  cp: string;
+  email: string;
+  geolocation: [string, string];
+  name: string;
+  phone: string;
+  website: string;
+}
