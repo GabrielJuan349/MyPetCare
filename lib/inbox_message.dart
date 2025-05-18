64d93d697f74b8661d608a0083bb12bfd04700a9
DaBM17 <93148240+DaBM17@users.noreply.github.com>
2025-05-19 01:59:43 +0200

    Update inbox_message.dart

--- lib/inbox_message.dart
+++ lib/inbox_message.dart
@@ -2,3 +2,8 @@ class InboxMessage {
   late String title, message;
   InboxMessage(this.title, this.message);
 }
+
+List<InboxMessage> myMessages = [
+  InboxMessage("Welcome to MyPetCare", "Thanks for choosing us"),
+  InboxMessage("Medication added", "A new Medication has been added to your pet"),
+];
