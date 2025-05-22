be031326a146fce399c75cc1aceb8c82305cc4b2
DaBM17 <93148240+DaBM17@users.noreply.github.com>
2025-05-22 23:32:49 +0200

    Update inbox_message.dart

--- lib/inbox_message.dart
+++ lib/inbox_message.dart
@@ -1,9 +1,15 @@
+//import 'dart:ffi';
+
 class InboxMessage {
-  late String title, message;
-  InboxMessage(this.title, this.message);
+  late String title, message, type, id;
+  bool read = false;
+  InboxMessage(this.title, this.message, this.type, this.id, this.read);
 }
 
+
+
 List<InboxMessage> myMessages = [
-  InboxMessage("Welcome to MyPetCare", "Thanks for choosing us"),
-  InboxMessage("Medication added", "A new Medication has been added to your pet"),
+  InboxMessage("Welcome to MyPetCare", "Thanks for choosing us", "report", "firstID", false),
+  InboxMessage("Medication added", "A new Medication has been added to your pet", "treatment", "secondID", false),
 ];
+
